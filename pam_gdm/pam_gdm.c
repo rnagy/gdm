0646c6c058afe58cc0bca6c229b38fb62728ae49
Charlie Root <robert@openbsd.org>
2021-06-28 09:09:04 +0200

    include linux specific pam headers on linux only

--- pam_gdm/pam_gdm.c
+++ pam_gdm/pam_gdm.c
@@ -21,11 +21,15 @@
 
 #include <unistd.h>
 
+#ifdef __linux__
 #include <security/_pam_macros.h>
 #include <security/pam_ext.h>
 #include <security/pam_misc.h>
 #include <security/pam_modules.h>
 #include <security/pam_modutil.h>
+#else
+#include <security/pam_modules.h>
+#endif
 
 #ifdef HAVE_KEYUTILS
 #include <keyutils.h>
